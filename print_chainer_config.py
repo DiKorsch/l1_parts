c577ff684862d2d15bd62b5beb3c788a15dd1a97
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2019-11-05 17:54:01 +0100

    updated config printing script

--- print_chainer_config.py
+++ print_chainer_config.py
@@ -2,7 +2,11 @@
 if __name__ != '__main__': raise Exception("Do not import me!")
 
 import chainer
-
 print(f"Chainer version: {chainer.__version__}")
-# import cupy
-# cupy.show_config()
+
+try:
+	import cupy
+	cupy.show_config()
+except Exception as e:
+	print("cupy could not be imported! Do you work in NVIDIA environment?!")
+	pass
