d890c9c395ca6e9cd89282aa5a8beae62fbb45c4
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2019-11-05 17:39:24 +0100

    fixed scripts for travis ci

--- print_chainer_config.py
+++ print_chainer_config.py
@@ -2,7 +2,7 @@
 if __name__ != '__main__': raise Exception("Do not import me!")
 
 import chainer
-import cupy
 
 print(f"Chainer version: {chainer.__version__}")
-cupy.show_config()
+# import cupy
+# cupy.show_config()
