a89011d4a6faf7c0f4dda48c72df9eb6294d8879
Dimitri Korsch <korschdima+github@gmail.com>
2020-08-10 10:10:49 +0200

    Update 01_install.sh

--- 01_install.sh
+++ 01_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source 00_config.sh
 
-docker build -f Dockerfile2 --tag chainer-cuda101-opencv4.1.1 .
+docker build -f Dockerfile2 --tag dikorsch/chainer-cuda101-opencv4.1.1 .
 
 docker-compose build $@ && docker-compose config
